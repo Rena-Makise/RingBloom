ed92dd76ef6d8e955999da3825a0a9dccdc42042
Rena <ckdwomn@live.com>
2019-10-27 21:06:50 +0900

    Initial commit

---
+++ src/main/java/ringbloom/RingBloomApplication.java
@@ -0,0 +1,13 @@
+package ringbloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingBloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingBloomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ringbloom/RingBloomApplicationTests.java
@@ -0,0 +1,13 @@
+package ringbloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingBloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
